ee7cfb78382f263cfd7d97a21992d8eedcca46f6
gallegosn <76745997+gallegosn@users.noreply.github.com>
2021-02-22 14:58:30 -0800

    updated hints and form validation

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -66,6 +66,19 @@ checkboxesFieldSet.addEventListener('change', e => {
         }
     totalDollars.innerHTML = `Total: $${totalCost}`;
     // code below borrowed (variables modified by me) from TimothyHBourne https://github.com/timothyhbourne/The_Interactive_Form/blob/main/js/script.js
+    //Activities focus and blur indicator
+    let checkedActivities = 0;
+    for (let i = 0; i < checkboxes.length; i++) {
+        let label = checkboxes[i].parentNode;
+        if (checkboxes[i].checked) {
+        checkedActivities += 1; 
+        label.classList.add('focus');
+        } else {
+        label.classList.remove('focus');
+        checkedActivities -= 1;
+        }
+    }
+    // Code prevents activities from being scheduled at the same time.
     const dataDayTime = e.target.getAttribute('data-day-and-time');
     const eventName = e.target.getAttribute('name');
     for (let i = 0; i < checkboxes.length; i++) {
@@ -84,9 +97,6 @@ checkboxesFieldSet.addEventListener('change', e => {
     // End Borrowed Code from TimothyHBourne
         
 });
-const hint = document.querySelector('#activities-hint');
-console.log(hint);
-hint.hidden = false;
 
 const paymentMethod = document.getElementById('payment');
 const creditCard = document.getElementById('credit-card');
@@ -95,6 +105,10 @@ const bitcoin = document.getElementById('bitcoin');
 
 payPal.hidden = true;
 bitcoin.hidden = true;
+creditCard.hidden = true;
+let cc = false;
+let bc = false;
+let pp = false;
 
 paymentMethod.addEventListener('change', e => {
     const clicked = e.target.value;
@@ -102,94 +116,126 @@ paymentMethod.addEventListener('change', e => {
         payPal.hidden = false;
         creditCard.hidden = true;
         bitcoin.hidden = true;
+        pp = true;
     }
     if (clicked == 'bitcoin'){
         payPal.hidden = true;
         creditCard.hidden = true;
         bitcoin.hidden = false;
-    }if (clicked == 'credit-card') {
+        bc = true;
+    }
+    if (clicked == 'credit-card') {
         payPal.hidden = true;
         creditCard.hidden = false;
         bitcoin.hidden = true;
+        cc = true;
     }
 });
 
 const cardNumber = document.getElementById('cc-num');
 const zipCode = document.getElementById('zip');
 const cvv = document.getElementById('cvv');
-const form = document.forms;
-    
+const form = document.querySelector('form');
+const hint = document.getElementById('activities-hint');
 
-form[0].addEventListener('click', e => {
-const typedName = document.getElementById('name');
-const typedNameValue = typedName.value;
-const nameRegEx = /^[a-z ,.'-]+$/i  //regex borrowed from stack overflow: https://stackoverflow.com/questions/2385701/regular-expression-for-first-and-last-nam
-if(typedNameValue != ''){
-    if (nameRegEx.test(typedNameValue)){      
-        typedName.parentElement.className = 'valid';
-        typedName.parentElement.lastElementChild.hidden = true;
+form.addEventListener('submit', (e) => {
+    if (cc == false && bc == false && pp == false){
+        e.preventDefault();
     }
-    else {
+    if (totalCost == 0){
+        e.preventDefault();
+        hint.className = 'not-valid';
+    }
+    if (totalCost != 0){
+        hint.className = 'valid';
+    }
+    const typedName = document.getElementById('name');
+    const typedNameValue = typedName.value;
+    const nameRegEx = /^[a-z ,.'-]+$/i  //regex borrowed from stack overflow: https://stackoverflow.com/questions/2385701/regular-expression-for-first-and-last-nam
+    if(typedNameValue != ''){
+        if (nameRegEx.test(typedNameValue)){      
+            typedName.parentElement.className = 'valid';
+            typedName.parentElement.lastElementChild.hidden = true;
+        }
+        else {
+            typedName.parentElement.className = 'not-valid';
+            typedName.parentElement.lastElementChild.className = 'not-valid';
+            typedName.parentElement.lastElementChild.hidden = false;
+            e.preventDefault();
+        }
+    }else{
         typedName.parentElement.className = 'not-valid';
         typedName.parentElement.lastElementChild.className = 'not-valid';
         typedName.parentElement.lastElementChild.hidden = false;
+        e.preventDefault();
     }
-}
-const typedEmail = document.getElementById('email');
-const typedEmailVal = typedEmail.value;
-const emailRegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // regex borrowed from stack overflow: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-if(typedEmailVal != ''){
-    if (emailRegEx.test(typedEmailVal)){
-        typedEmail.parentElement.className = 'valid';
-        typedEmail.parentElement.lastElementChild.hidden = true;
-    }
-    else{
+    const typedEmail = document.getElementById('email');
+    const typedEmailVal = typedEmail.value;
+    const emailRegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // regex borrowed from stack overflow: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+    if(typedEmailVal != ''){
+        if (emailRegEx.test(typedEmailVal)){
+            typedEmail.parentElement.className = 'valid';
+            typedEmail.parentElement.lastElementChild.hidden = true;
+        }
+        else{
+            typedEmail.parentElement.className = 'not-valid';
+            typedEmail.parentElement.lastElementChild.className = 'not-valid';
+            typedEmail.parentElement.lastElementChild.hidden = false;
+            e.preventDefault();
+        }
+    }else{
         typedEmail.parentElement.className = 'not-valid';
         typedEmail.parentElement.lastElementChild.className = 'not-valid';
         typedEmail.parentElement.lastElementChild.hidden = false;
+        e.preventDefault();
     }
-}
-const cardNumVal = cardNumber.value;
-const ccRegEx = /(?:(\d)[ -]?){12,15}(\d)/ // regex (13-16 digits) borrowed from stack overflow: https://stackoverflow.com/questions/57290428/create-credit-card-regex
-if(cardNumVal != 0){
-    if (ccRegEx.test(cardNumVal)){
-        cardNumber.parentElement.className = 'valid';
-        cardNumber.parentElement.lastElementChild.hidden = true;
-    }
-    else{
+    const cardNumVal = cardNumber.value;
+    const ccRegEx = /(?:(\d)[ -]?){12,15}(\d)/ // regex (13-16 digits) borrowed from stack overflow: https://stackoverflow.com/questions/57290428/create-credit-card-regex
+    if(cardNumVal != 0 || cc == true){
+        if (ccRegEx.test(cardNumVal)){
+            cardNumber.parentElement.className = 'valid';
+            cardNumber.parentElement.lastElementChild.hidden = true;
+        }
+        else{
+            cardNumber.parentElement.className = 'not-valid';
+            cardNumber.parentElement.lastElementChild.className = 'not-valid';
+            cardNumber.parentElement.lastElementChild.hidden = false;
+            e.preventDefault();
+        }
+    }else if (cc == true){
         cardNumber.parentElement.className = 'not-valid';
         cardNumber.parentElement.lastElementChild.className = 'not-valid';
         cardNumber.parentElement.lastElementChild.hidden = false;
+        e.preventDefault();
     }
-}
-
-const zipCodeVal = zipCode.value;
-const zipRegEx = /\d{5}/i
-if(zipCodeVal != 0){
-    if (zipRegEx.test(zipCodeVal)){
-        zipCode.parentElement.className = 'valid';
-        zipCode.parentElement.lastElementChild.hidden = true;
+    const zipCodeVal = zipCode.value;
+    const zipRegEx = /\d{5}/i
+    if(zipCodeVal != 0 || cc == true){
+        if (zipRegEx.test(zipCodeVal)){
+            zipCode.parentElement.className = 'valid';
+            zipCode.parentElement.lastElementChild.hidden = true;
+        }
+        else{
+            zipCode.parentElement.className = 'not-valid';
+            zipCode.parentElement.lastElementChild.className = 'not-valid';
+            zipCode.parentElement.lastElementChild.hidden = false;
+            e.preventDefault()
+        }
     }
-    else{
-        zipCode.parentElement.className = 'not-valid';
-        zipCode.parentElement.lastElementChild.className = 'not-valid';
-        zipCode.parentElement.lastElementChild.hidden = false;
+    const cvvVal = cvv.value;
+    const cvvRegEx = /\d{3}/i
+    if(cvvVal != 0 || cc == true){
+        if (cvvRegEx.test(cvvVal)){
+            cvv.parentElement.className = 'valid';
+            cvv.parentElement.lastElementChild.hidden = true;
+        }
+        else{
+            cvv.parentElement.className = 'not-valid';
+            cvv.parentElement.lastElementChild.className = 'not-valid';
+            cvv.parentElement.lastElementChild.hidden = false;
+            e.preventDefault();
+        }
     }
-}
-const cvvVal = cvv.value;
-const cvvRegEx = /\d{3}/i
-if(cvvVal != 0){
-    if (cvvRegEx.test(cvvVal)){
-        cvv.parentElement.className = 'valid';
-        cvv.parentElement.lastElementChild.hidden = true;
-    }
-    else{
-        cvv.parentElement.className = 'not-valid';
-        cvv.parentElement.lastElementChild.className = 'not-valid';
-        cvv.parentElement.lastElementChild.hidden = false;
-    }
-}
-
 });
 
 
